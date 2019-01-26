8ffa08080f21fdbe4a9d6ee323b9467b45e202cb
EthanFischer <ethan.f.fischer@gmail.com>
2019-01-26 17:49:42 -0600

    Cupboard button shows cupboard

---
+++ HomeGame/Assets/ShowCupboard.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class ShowCupboard : MonoBehaviour
+{
+    public GameObject CupboardUI;
+
+    public void ShowIt()
+    {
+        CupboardUI.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
