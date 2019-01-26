63d081860f7964fab054bd0bf1017f876d202f8c
EthanFischer <ethan.f.fischer@gmail.com>
2019-01-26 16:58:48 -0600

    Update dialogmanager to always do current dialogue

---
+++ HomeGame/Assets/Scripts/DialogueManagers/DialogueManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+	public VIDE_Assign[] Dialogues;
+	public Template_UIManager UIManager;
+	public SceneManager SceneManager;
+
+	public virtual void DoDialogue()
+	{
+		UIManager.Interact(CurrentDialogue());
+	}
+
+    protected virtual VIDE_Assign CurrentDialogue()
+    {
+        int index = SceneManager.SceneIndex;
+        return Dialogues[index];
+    }
+}
