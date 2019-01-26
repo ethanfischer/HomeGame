25bb1214f13fd82e7eaa7803e90f4a0663ff9266
P-Wallis <P-Wallis@users.noreply.github.com>
2019-01-26 16:32:03 -0600

    Added DialogueManager component and set up basic manage-y stuff

---
+++ HomeGame/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+	public VIDE_Assign[] Dialogues;
+	public Template_UIManager Manager;
+
+	public void DoDialogue()
+	{
+		int index = 0;
+
+		// Something that finds the proper index
+
+		Manager.Interact(Dialogues[index]);
+	}
+}
